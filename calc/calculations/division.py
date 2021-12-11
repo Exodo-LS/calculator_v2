c8387f3d4aff6676b3061880c14435da6fa0a928
Mohammad-Shamel Agha <ma2298@njit.edu>
2021-12-10 19:14:54 -0500

    Task Update

---
+++ calc/calculations/division.py
@@ -0,0 +1,11 @@
+"""Division Class"""
+from calc.calculations.calculation import Calculation
+
+class Division(Calculation):
+    """division calculation object"""
+    def get_result(self):
+        """get the division results"""
+        value_div = 1.0
+        for value in self.values:
+            value_div = (value_div / value) if value != 0 else "Divide By Zero Detected"
+        return value_div
